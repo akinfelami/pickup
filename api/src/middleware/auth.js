a9293cccba97595f1b2b203cec14f48a66dd3d6b
aoa9 <aoa9@cornell.edu>
2022-08-05 00:43:42 -0500

    auth setup

--- api/src/middleware/auth.js
+++ api/src/middleware/auth.js
@@ -1,2 +1,19 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+	const token = req.cookies.jwt;
+
+	if (!token) {
+		return res.status(403).send('Unauthorized Access');
+	}
+	try {
+		const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+		req.user = decoded;
+		next();
+	} catch (err) {
+		return res.status(401).send('Invalid Toekn');
+	}
+};
+
+module.exports = verifyToken;
---
+++ api/src/routes/user.js
@@ -0,0 +1,107 @@
+const express = require('express');
+const router = express.Router();
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
+const User = require('../models/User');
+const auth = require('../middleware/auth');
+
+router.get('/:id', auth, async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id).lean().exec();
+		res.status(200).json({ name: user.username, email: user.email });
+	} catch (err) {
+		console.error(err);
+		res.status(400).json({ status: 'failed' });
+	}
+});
+
+router.post('/register', async (req, res) => {
+	try {
+		const { username, email, password } = req.body;
+
+		if (!(email && password && username)) {
+			res.status(400).send('All input required');
+		}
+
+		const oldUser = await User.findOne({ email });
+
+		if (oldUser) {
+			res.status(409).send('User already exists. Please Login instead');
+		}
+
+		var encryptedPassword = await bcrypt.hash(password, 10);
+
+		const newUser = await User.create({
+			username,
+			email: email.toLowerCase(),
+			password: encryptedPassword,
+		});
+
+		// Create Tokens
+
+		const token = jwt.sign(
+			{ user_id: newUser._id, email },
+			process.env.TOKEN_KEY
+		);
+
+		res.cookie('jwt', token, { httpOnly: true });
+
+		res.status(201).json({
+			id: newUser._id,
+			username: newUser.username,
+			email: newUser.email,
+			profile_pic: newUser.profile_pic,
+			interests: newUser.interests,
+			createdAt: newUser.createdAt,
+		});
+	} catch (err) {
+		console.error(err);
+		res.status(400).json({ err });
+	}
+});
+
+router.post('/login', async (req, res) => {
+	try {
+		const { email, password } = req.body;
+		if (!(email && password)) {
+			res.status(400).send('All input required');
+		}
+
+		const user = await User.findOne({ email });
+		if (user && (await bcrypt.compare(password, user.password))) {
+			const token = jwt.sign(
+				{ user_id: user._id, email },
+				process.env.TOKEN_KEY
+			);
+
+			res.cookie('jwt', token, { httpOnly: true });
+
+			res.status(200).json({
+				id: user._id,
+				username: user.username,
+				email: user.email,
+				profile_pic: user.profile_pic,
+				interests: user.interests,
+				createdAt: user.createdAt,
+			});
+		} else {
+			res.status(400).send('Invalid Credentials');
+		}
+	} catch (err) {
+		console.error(err);
+	}
+});
+
+router.get('/logout', auth, async (req, res) => {
+	const authHeader =
+		req.body.token || req.query.token || req.headers['x-access-token'];
+
+	try {
+		res.cookie('jwt', '', { maxAge: 1 });
+		res.send({ msg: 'You have been logged out' });
+	} catch (err) {
+		res.send({ msg: err });
+	}
+});
+
+module.exports = router;
