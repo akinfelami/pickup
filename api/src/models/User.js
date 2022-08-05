b8e83da659c4b80823389555d2da9bca1bd8ecd5
aoa9 <aoa9@cornell.edu>
2022-08-05 00:43:42 -0500

    auth setup

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
-var bcrypt = require('bcryptjs');
 const path = require('path');
 const y = path.resolve('src', 'images', 'profileimg.png');
 
 const userSchema = new mongoose.Schema(
 	{
-		username: { type: String },
+		username: { type: String, required: true },
 		email: { type: String, required: true, unique: true },
 		password: { type: String, required: true },
 		profile_pic: { type: String, default: y },
@@ -28,14 +27,6 @@ const userSchema = new mongoose.Schema(
 		timestamps: true,
 	}
 );
-
-userSchema.pre('save', function (next) {
-	if (!this.isModified('password')) return next();
-
-	var hash = bcrypt.hashSync(this.password, 8);
-	this.password = hash;
-	return next();
-});
 
 userSchema.methods.checkPassword = function (password) {
 	return bcrypt.compareSync(password, this.password);
--- api/src/server.js
+++ api/src/server.js
@@ -3,11 +3,13 @@ const cors = require('cors');
 require('dotenv').config();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 3000;
 const app = express();
+const userRouter = require('./routes/user');
 
 app.use(cors());
-
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -21,16 +23,25 @@ const connectDb = async () => {
 	}
 };
 
+// Router Middleware
+app.use('/', userRouter);
+
 app.get('/', (req, res) => {
 	res.send("<h1 style='text-align:center; margin-top:50px'>We are live!</h1>");
+});
+
+app.use('*', (req, res) => {
+	res.status(404).json({
+		success: 'false',
+		message: 'Page not found',
+		error: {
+			statusCode: 404,
+			message: 'You reached a route that is not defined on this server',
+		},
+	});
 });
 
 app.listen(port, function () {
 	connectDb();
 	console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-});
-
-process.on('SIGINT', () => {
-	console.log('Bye bye!');
-	process.exit();
 });
