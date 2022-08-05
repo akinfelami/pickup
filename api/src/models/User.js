d757bff44ac4924b7514b8fa39701ba072a5ce4f
aoa9 <aoa9@cornell.edu>
2022-08-05 00:57:38 -0500

    auth setup

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -28,9 +28,5 @@ const userSchema = new mongoose.Schema(
 	}
 );
 
-userSchema.methods.checkPassword = function (password) {
-	return bcrypt.compareSync(password, this.password);
-};
-
 const User = mongoose.model('user', userSchema);
 module.exports = User;
