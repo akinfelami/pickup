d5b8490033d07361b3cb09b9bedeab7c05738353
aoa9 <aoa9@cornell.edu>
2022-08-11 03:25:03 -0500

    configure redis env

--- api/src/utils/redis.js
+++ api/src/utils/redis.js
@@ -2,8 +2,16 @@ const mongoose = require('mongoose');
 const redis = require('redis');
 const util = require('util');
 require('dotenv').config();
-const client = redis.createClient(process.env.REDIS_URL);
-client.on('error', (err) => console.log('Redis Client Error', err));
+const client = redis.createClient({
+	socket: {
+		host: process.env.REDIS_HOST,
+		port: process.env.REDIS_PORT,
+	},
+	password: process.env.REDIS_PASSWORD,
+});
+client.on('error', (err) => {
+	console.log('Error ' + err);
+});
 
 const connectRedis = async () => {
 	try {
