9079941eba1911f3bd7d3165d8e799986e58d29e
aoa9 <aoa9@cornell.edu>
2022-08-15 18:44:23 -0400

    set up firebase auth

--- api/src/modules/users/UserRoutes.js
+++ api/src/modules/users/UserRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../../middleware/auth');
+const { auth } = require('../../middleware/auth');
 const userController = require('./UserController');
 
 router.get('/', auth, userController.getWelcome);
