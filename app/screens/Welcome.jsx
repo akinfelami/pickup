3b7bc5538d8acadcd2209a76e5912a9d38e296ef
aoa9 <aoa9@cornell.edu>
2022-08-17 14:28:19 -0400

    Initialize home page

--- app/screens/Welcome.jsx
+++ app/screens/Welcome.jsx
@@ -7,24 +7,29 @@ import React, { useEffect } from 'react';
 
 const Welcome = ({ navigation }) => {
 	useEffect(() => {
-		if (auth.currentUser) {
-			navigation.replace('Home');
-		} else {
-			navigation.replace('Login');
-		}
+		const unsubscribe = onAuthStateChanged(auth, (authUser) => {
+			if (authUser) {
+				navigation.replace('Home');
+			} else {
+				navigation.replace('Login');
+			}
+		});
+		return unsubscribe;
 	}, []);
 
 	return (
 		<SafeAreaView className='flex-1 justify-center items-center h-screen'>
 			<View className='flex-4'>
 				<Image
-					source={require('../assets/pickup.png')}
+					source={{
+						uri: 'https://firebasestorage.googleapis.com/v0/b/cornell-pickup.appspot.com/o/pickup.png?alt=media&token=511330b4-fa98-4dc6-aefe-4a4cda2e77ef',
+					}}
 					style={{ width: 200, height: 200 }}
 				/>
 				{/* Commented out code used for testing */}
-				{/* <Button
+				<Button
 					title='Login'
-					onPress={() => navigation.replace('Login')}></Button> */}
+					onPress={() => navigation.replace('Login')}></Button>
 			</View>
 		</SafeAreaView>
 	);
