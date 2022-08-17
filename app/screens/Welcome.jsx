655ffd058aded7bbbe63f88daf16fc891c4577d5
aoa9 <aoa9@cornell.edu>
2022-08-17 13:58:40 -0400

    auth working

---
+++ app/screens/Welcome.jsx
@@ -0,0 +1,36 @@
+import { SafeAreaView, View } from 'react-native';
+import { Image, Text, Button } from 'react-native-elements';
+import { auth } from './../firebase';
+import { onAuthStateChanged } from 'firebase/auth';
+
+import React, { useEffect } from 'react';
+
+const Welcome = ({ navigation }) => {
+	useEffect(() => {
+		const unsubscribe = onAuthStateChanged(auth, (authUser) => {
+			if (authUser) {
+				navigation.replace('Home');
+			} else {
+				navigation.replace('Login');
+			}
+		});
+		return unsubscribe;
+	}, []);
+
+	return (
+		<SafeAreaView className='flex-1 justify-center items-center h-screen'>
+			<View className='flex-4'>
+				<Image
+					source={require('../assets/pickup.png')}
+					style={{ width: 200, height: 200 }}
+				/>
+				{/* Commented out code used for testing */}
+				{/* <Button
+					title='Login'
+					onPress={() => navigation.replace('Login')}></Button> */}
+			</View>
+		</SafeAreaView>
+	);
+};
+
+export default Welcome;
