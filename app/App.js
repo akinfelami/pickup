888f0396e7d3b764107f18a086e78133ff1485f1
aoa9 <aoa9@cornell.edu>
2022-08-17 20:22:32 -0400

    debug expo not responding

--- app/App.js
+++ app/App.js
@@ -16,12 +16,12 @@ export default function App() {
 		<NavigationContainer>
 			<TailwindProvider>
 				<Stack.Navigator>
+					<Stack.Screen name='Login' component={Login} />
 					<Stack.Screen
 						options={{ headerShown: false }}
 						name='Welcome'
 						component={Welcome}
 					/>
-					<Stack.Screen name='Login' component={Login} />
 					<Stack.Screen name='Register' component={Register} />
 					<Stack.Screen
 						options={{ headerShown: false }}
