53db00493e778f527d29031f1a3fc44a3812950b
aoa9 <aoa9@cornell.edu>
2022-08-17 23:22:19 -0400

    dynamically get user's details on homepage load

--- app/screens/Home.jsx
+++ app/screens/Home.jsx
@@ -2,11 +2,14 @@ import { View, SafeAreaView, TouchableOpacity } from 'react-native';
 import { Image, Text, Button } from 'react-native-elements';
 import { auth } from './../firebase';
 import { signOut } from 'firebase/auth';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { apiBaseUrl } from './../constants';
+import axios from 'axios';
 
 const Home = ({ navigation }) => {
 	let userData;
+
+	const [username, setUsername] = useState('User');
 
 	const signOutUser = async () => {
 		try {
@@ -17,22 +20,23 @@ const Home = ({ navigation }) => {
 		}
 	};
 
-	useEffect(async () => {
+	useEffect(() => {
+		fetchUser();
+	}, []);
+
+	const fetchUser = async () => {
 		try {
-			const token = auth.currentUser.getIdToken();
+			const token = await auth.currentUser.getIdToken();
 			const response = await fetch(
 				`${apiBaseUrl}/user/get/${auth.currentUser.uid}`,
-				{
-					headers: {
-						Authorization: `Bearer ${token}`,
-					},
-				}
+				{ headers: { authorization: `Bearer ${token}` } }
 			);
 			userData = await response.json();
+			setUsername(userData.displayName);
 		} catch (err) {
 			console.error(err);
 		}
-	}, []);
+	};
 
 	return (
 		<SafeAreaView className='flex-1'>
@@ -43,13 +47,8 @@ const Home = ({ navigation }) => {
 						style={{ width: 70, height: 70 }}
 					/>
 				</TouchableOpacity>
-				<View>
-					{userData === undefined ? (
-						<Text h4>Hi, User!</Text>
-					) : (
-						<Text h4>Hi, {userData.displayName}</Text>
-					)}
-				</View>
+
+				<Text h3>Hi {username}</Text>
 			</View>
 		</SafeAreaView>
 	);
