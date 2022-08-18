ffd54bc9e6fa98cd9d5ef32aa06318f044709ae2
aoa9 <aoa9@cornell.edu>
2022-08-17 21:31:49 -0400

    activity indicator

--- app/screens/Login.jsx
+++ app/screens/Login.jsx
@@ -5,6 +5,7 @@ import {
 	TouchableWithoutFeedback,
 	Keyboard,
 	SafeAreaView,
+	ActivityIndicator,
 } from 'react-native';
 import { Image, Text, Input, Button } from 'react-native-elements';
 import React, { useState, useEffect } from 'react';
@@ -16,17 +17,6 @@ import LoadingComponent from '../components/LoadingComponent';
 
 const Login = ({ navigation }) => {
 	const [loading, setIsLoading] = useState(false);
-
-	// useEffect(() => {
-	// 	const unsubscribe = onAuthStateChanged(auth, (authUser) => {
-	// 		if (authUser) {
-	// 			navigation.replace('Home');
-	// 		} else {
-	// 			navigation.replace('Login');
-	// 		}
-	// 	});
-	// 	return unsubscribe;
-	// }, []);
 
 	useEffect(() => {
 		const unsub = onAuthStateChanged(auth, (authUser) => {
@@ -43,9 +33,11 @@ const Login = ({ navigation }) => {
 	const height = useHeaderHeight();
 
 	const loginUser = () => {
+		setIsLoading(true);
 		signInWithEmailAndPassword(auth, email, password)
 			.then((userCredential) => {
 				const user = userCredential.user;
+				setIsLoading(false);
 			})
 			.catch((error) => {
 				alert(error.code);
@@ -57,63 +49,65 @@ const Login = ({ navigation }) => {
 			keyboardVerticalOffset={height + 64}
 			behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
 			className='flex-1'>
-			{!loading && (
-				<SafeAreaView className='flex-1'>
-					<TouchableWithoutFeedback
-						onPress={Keyboard.dismiss}
-						accessible={false}>
-						<View>
-							<View className='justify-items-center items-center flex-1 pt-8'>
-								<Image
-									source={require('../assets/pickup.png')}
-									style={{ width: 300, height: 300 }}
+			<SafeAreaView className='flex-1'>
+				<TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+					<View>
+						<View className='justify-items-center items-center flex-1 pt-8'>
+							<Image
+								source={require('../assets/pickup.png')}
+								style={{ width: 300, height: 300 }}
+							/>
+
+							<View style={{ width: 350 }}>
+								<Input
+									textContentType='username'
+									placeholder='Email'
+									autoFocus
+									type='email'
+									value={email}
+									onChangeText={(text) => setEmail(text)}
 								/>
 
-								<View style={{ width: 350 }}>
-									<Input
-										textContentType='username'
-										placeholder='Email'
-										autoFocus
-										type='email'
-										value={email}
-										onChangeText={(text) => setEmail(text)}
-									/>
-
-									<Input
-										placeholder='Password'
-										textContentType='password'
-										secureTextEntry
-										type='password'
-										value={password}
-										onChangeText={(text) => setPassword(text)}
-										onSubmitEditing={loginUser}
-									/>
-								</View>
+								<Input
+									placeholder='Password'
+									textContentType='password'
+									secureTextEntry
+									type='password'
+									value={password}
+									onChangeText={(text) => setPassword(text)}
+									onSubmitEditing={loginUser}
+								/>
+							</View>
+							{loading === true ? (
+								<Button loading title='Login' style={{ width: 200 }} />
+							) : (
 								<Button
+									raised
 									onPress={loginUser}
 									title='Login'
 									style={{ width: 200 }}
 								/>
-								<View className='pt-4 items-center flex-row space-x-1'>
-									<Text>Don't have an account? </Text>
-									<TouchableOpacity>
-										<Link to={{ screen: 'Register' }}>
-											<Text className='underline text-sky-400'>
-												Register here
-											</Text>
-										</Link>
-									</TouchableOpacity>
-								</View>
-								<Text className='pt-2 underline text-sky-400'>
-									Forgot Password
-								</Text>
+							)}
 
-								<View style={{ flex: 1 }} />
+							<View className='pt-4 items-center flex-row space-x-1'>
+								<Text>Don't have an account? </Text>
+								<TouchableOpacity>
+									<Link to={{ screen: 'Register' }}>
+										<Text className='underline text-sky-400'>
+											Register here
+										</Text>
+									</Link>
+								</TouchableOpacity>
 							</View>
+							<Text className='pt-2 underline text-sky-400'>
+								Forgot Password
+							</Text>
+
+							<View style={{ flex: 1 }} />
 						</View>
-					</TouchableWithoutFeedback>
-				</SafeAreaView>
-			)}
+					</View>
+				</TouchableWithoutFeedback>
+			</SafeAreaView>
 		</KeyboardAvoidingView>
 	);
 };
