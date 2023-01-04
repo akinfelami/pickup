b81186ce7f2de543e772c2b4a97838c7ad79f060
aoa9 <aoa9@cornell.edu>
2023-01-04 16:45:59 -0600

    i give up

--- app/App.js
+++ app/App.js
@@ -69,6 +69,8 @@ export default function App() {
 						component={LoginScreen}
 					/>
 
+					<Stack.Screen name='EventDetails' component={EventDetails} />
+
 					<Stack.Screen name='Register' component={RegisterScreen} />
 				</Stack.Navigator>
 			</TailwindProvider>
