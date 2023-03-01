924da8ecb3a4b0f728dce632b4640dbf3580d69f
aoa9 <aoa9@cornell.edu>
2023-03-01 15:03:40 -0500

    it's a new era

--- app/constants.js
+++
@@ -1,1 +0,0 @@
-export const apiBaseUrl = 'https://pickupapi.herokuapp.com';
---
+++ client/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
